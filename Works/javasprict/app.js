7bffcd1c46a3437740556ad72061029a2d0d3662
omarseyyad <seyyad_omerov@mail.ru>
2021-10-20 14:29:36 +0400

    works

--- Works/javasprict/app.js
+++ Works/javasprict/app.js
@@ -1,4 +1,17 @@
-let mesaj;
-mesaj = 'Hello!';
+// let cumle=`adxsafdbsufcfrgə  gdfggedgə g vgcfsegdxəgrgvvrgs grəgəəəəəəəfetetcr h tretəretretəsfdcdsəəə`
+// let ad='samirkerimov';
 
-alert(mesaj); // değişkenin içeriğini gösterir.
+// for(let i=0;i<cumle.length;i++){
+//   if(cumle[i]=='ə'){
+//     console.log('ə herfi tapdim')
+//    }
+// }
+
+// İstifadəci ekrandan 1 rəqəmini daxil edəndə 1-100 arası ədədlərin cəmini ekrana cap edin
+function eded(2){
+
+}
+let eded=prompt('Ededi daxil edin:')
+
+  alert(eded)
+}

ce30e7932dbd81d2d7b412bc36f9e4af0671b04d
omarseyyad <seyyad_omerov@mail.ru>
2021-10-21 23:03:53 +0400

    yeniledim

--- Works/javasprict/app.js
+++ Works/javasprict/app.js
@@ -24,3 +24,67 @@
 // console.log(30)
 // }
 // bax()
+
+// let girisadi=prompt('adiniz:')
+// if(girisadi=='seyyad')
+// {
+
+//     let vezife=prompt('Vezifeniz')
+//     if(vezife=="editor")
+//     {
+//         alert('hormetli seyyad siz siyaset bolmesine baxa bilersiniz')
+//     }else{
+//        if (vezife=="admin"){
+//             alert('hormetli seyyad siz butun bolmelere  baxa bilersiniz')
+//     }else{alert('hormetli seyyad bele xidmet movcud deyil')}
+//     }
+// }
+
+// let a = prompt('yasinizi daxil edin:')
+// if (a < 15 || a > 75) {
+//     alert('siz usaq xasiyyetlisiniz')
+// }
+// else {
+//     if (a > 15 && a < 45) {
+//         alert('ciddi ol biraz')
+//     }
+//     else {
+//         if (a > 45 && a < 75) {
+//             alert('uzuc kefen sayti')
+//         }
+//     }
+// }
+
+/*prompt vasitəsi ilə aşağıdakı məlumatlar tələb olunur
+
+istifadəçinin adi
+istifadəçinin soyadı Tələb olunanlar
+console ekraninda ad iki dəfə soyad isə 4 dəfə cap edilsin
+consoleda ad ||| soyad formasinda yazi cap edilsin (numune : Samir ||| Kerimov)
+consoleda Sizin adiniz : ad , Sizin soyadiniz : soyad (numune : Sizin adiniz: Samir,Sizin soyadiniz: Kerimov)*/
+
+// let ad=prompt('Istifadecinin Adi:')
+// let soyad=prompt('Istifadecinin Soyadi:')
+//  console.log(ad,soyad)
+
+
+//  let ad=prompt('Istifadecinin Adi:')
+//  let soyad=prompt('Istifadecinin Soyadi:')
+//  for (let i = 1; i <3 ; i++){ 
+//   console.log(ad)
+//  }
+//  for (let i = 1; i <5 ; i++){ 
+//     console.log(soyad)
+//    }
+
+
+
+//  let ad=prompt('Istifadecinin Adi:')
+// let soyad=prompt('Istifadecinin Soyadi:')
+// console.log(ad,'|||',soyad)
+
+
+//   let ad=prompt('Istifadecinin Adi:')
+//  let soyad=prompt('Istifadecinin Soyadi:')
+//  let netice=(('Sizin adiniz:'+ ad)+(',') +('Sizin soyadiniz:'+soyad))
+//  console.log(netice)

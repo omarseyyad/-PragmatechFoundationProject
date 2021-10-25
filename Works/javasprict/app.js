8ac41d7499a8ab4ac61fc87cc1758fc88abad809
omarseyyad <seyyad_omerov@mail.ru>
2021-10-25 09:48:04 +0400

    yenilendi

--- Works/javasprict/app.js
+++ Works/javasprict/app.js
@@ -90,9 +90,148 @@ consoleda Sizin adiniz : ad , Sizin soyadiniz : soyad (numune : Sizin adiniz: Sa
 //  console.log(netice)
 
 
-let age=prompt('yasinizi daxil edin:')
-if(age>12){
-    alert('Saytimiza xos geldiniz')
-}else{
-    alert('Azyasli usaqlar ucun giris qadagandir!')
+// let age=prompt('yasinizi daxil edin:')
+// if(age>12){
+//     alert('Saytimiza xos geldiniz')
+// }else{
+//     alert('Azyasli usaqlar ucun giris qadagandir!')
+// }
+
+// let a=5
+// let b=true
+// let c='samir'
+// let d=undefined
+// let e=null
+
+// // console.log(a+d)//nan
+// console.log(a-d)//nan
+// console.log(a-d)
+// console.log(a-d)
+
+
+// let a=4
+// let b=7
+// a+=3
+// a*=5
+// a/=3
+// console.log(a)
+
+
+// let cumle='salam necesen dostum'
+// let ad=prompt('adinizi yazin')
+// ad+=
+
+// document.write('salam  //n necesen')
+
+// let ad1='seyyad'
+
+
+// let content=`${ad} xos gelmisen`
+// console.log(content)
+
+
+
+
+
+                         // TAPSIRIQ.Week05-Day05 [22 Oktyabr 2021]
+
+/* 1)2 ededi parametr olaraq qəbul edib ededlərin hasilini return edən funksiya yazın
+
+let hasil = eded(2,5);
+
+function eded(a,b) {
+  return a * b;
+
 }
+console.log(hasil)
+------------------------------------------
+ let cem=eded(20,10)
+ function eded(a,b){
+     return a+b
+ }
+ console.log(cem)
+
+let bolme=eded(10,2)
+function eded(a,b){
+    return a/b
+}console.log(bolme) */
+
+/*2) 1 eded parametr qəbul edib daxil edilən dəyərin 1 artığını return elətdirən funksiya yazın 
+let netice=eded(5)
+function eded(a){
+    return a
+}
+console.log(++netice) */
+
+/*3) il dəyəri daxil edildiyi zaman onun necə ay və necə gün olduğunu return edən funksiya yazın 
+
+let il=eded(prompt('ili daxil edin'))
+
+function eded(a){
+    return a*12
+}
+alert(il +'ay')
+function eded(a){
+    return a*365
+}
+alert(il +'gun')
+
+----------------------------------------------
+let il=prompt('ili daxil edin')
+ay=12*il
+gun=il*365
+console.log(ay + 'ay' + gun + 'gun') */
+
+/* 4)Parametr olaraq array qəbul edən və daxil edilən arrayın ilk elementini return edən funksiya yazın*/
+// let soz=alo([["Salam"], ["saqol"], ["helelik"]]);
+// function alo(a) {
+// return    alo= [0]
+// }
+//  alert(soz)
+
+
+// Verilən str = 'Proqramalaşdırma nə qədər çox şey bildiyinizlə yox,
+//  bildiyinizlə ortaya çıxardığınız işlərlə maraqlanır'
+
+/* 7)str daxilində neçə xarakter olduğunu ekrana yazdırın */
+
+
+/*  8) str daxilində neçə hərf olduğunu ekrana yazdırın 
+
+let str = "Proqramalaşdırma nə qədər çox şey bildiyinizlə yox,bildiyinizlə ortaya çıxardığınız işlərlə maraqlanır"
+alert(str.length)  */
+
+
+/*  9) str daxilindəki sözləri ayrı bir massiv içərisində toplayın */
+
+/* 10) str daxilində neçə sait və neçə samit olduğunu ekrana çap edin */
+
+/* 11) str daxilində son iki sözü silən metod yazın */
+
+/* 12) str ni vergülə görə ayırıb iki string halına gətirin */
+
+/* 13) stringSearch(word) adında bir metod yazın. daxil edilən sözün mətnin içində olub olmadığını ekrana çap edən metod yazın */
+
+
+// Verilən nums=[1,2,3,6,7,8,23,78,34,12]
+
+/* 14) Ədədlərin cəmini tapan metod yazın */
+ let nums=cem([1,2,3,6,7,8,23,78,34,12])
+function cem([a,b,c,d,e,f,g,h,i,x]){
+    return a+b+c+d+e+f+g+h+i+x
+}
+alert(nums)
+
+    
+
+/* 15) Ədədlərin böyükdən kiçiyə doğru sıralayın */
+
+/* 16) Ədədlər arasında rəqəmlərinin cəmi ən böyük olan ədədi tapın */
+
+/* 17) Ədədlərin kvadratlarının olduğu yeni bir massiv yaradan metod yazın */
+
+/* 18) Tək ədədlərin cəmini tapan metod yazın */
+
+/* 19) Daxilində 3 rəqəmi olan neçə ədəd olduğunu ekrana çap edən metod yazın */
+
+/* 20) Tək ədədləri ayıraraq ayrıca bir massivə yığan metod yazın */

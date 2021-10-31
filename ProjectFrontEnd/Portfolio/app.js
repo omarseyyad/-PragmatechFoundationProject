b9b23ec36b5692bc2e5d0ebfe7138f7e353f80ad
omarseyyad <seyyad_omerov@mail.ru>
2021-10-31 23:09:42 +0400

    HTML ve CSS tam bitdi

---
+++ ProjectFrontEnd/Portfolio/app.js
@@ -0,0 +1,62 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let reyler=[
+// {
+// basliq:"gsfdskxsibxfe "
+// detal:"wxd5tms3b,nz3mnygudbn4juhbf5unbe5mfdienr"
+// },
+// {
+// basliq:"deuifrb"
+// detal:"eddt5msb,td4nm mbnwsl3o,7gmsqtfgd .l.dl;xsploium4slbuesb"
+// },
+// {
+// basliq:"swenrmlbrebaew srfdel"
+// detal:"mdrubrs3rdrbnwlts.iqploj,nsl3iyh6emd6losw"
+// }
+// ]
+
+// for(let i=0; i<reyler.length;i++){
+//     let btn=
+// }
+
+// let basliqelementi=document.querySelector('.rowtestimonial h3')
+// let detalelementi=document.querySelector('.rowtestimonial p')
+
+// let i=0;
+// function reyyarat(_basliq,_detal){
+//     basliqelementi.innerHTML=_basliq
+//     detalelementi.innerHTML=_detal
+// }
+// let reyinterval=setinterval(function() {
+//     if
+    
+// }

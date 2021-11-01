39dea6dc823e48dceb924d83c905d61e8ea2cf26
omarseyyad <seyyad_omerov@mail.ru>
2021-11-02 00:00:30 +0400

    slayd

---
+++ Works/Testimonial/slayd.js
@@ -0,0 +1,19 @@
+let reyler=[
+    {
+        basliq:'Samir Kerimov',
+        detal:'gozel  djhvzndj unygvdzj yntzwv d ybnvdzuduv vwebvnaee vjueanvuq'
+    },
+    {
+        basliq:'Nezrin Isbendyarova',
+        detal:'Ela uqurlarin bol olsunxudm  djhvzndj unygvdzj yntzwv d ybnvdzuduv vwebvnaee vjueanvuq'
+    },
+    {
+        basliq:'Mehdi Cefer',
+        detal:'Supeeerrrrr djhvzndj unygvdzj yntzwv d ybnvdzuduv vwebvnaee vjueanvuq'
+    }
+
+
+ ]
+
+ let basliqElementi=document.querySelector('.rey-container h3')
+ let detalElementi=document.querySelector('.rey-container p')

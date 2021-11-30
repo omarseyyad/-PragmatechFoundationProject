5b37f027afe2b586fe55d4ba926d29effe052b90
omarseyyad <seyyad_omerov@mail.ru>
2021-11-30 22:44:12 +0400

    python-day-2

--- Works/Python/Python-day-02.py
+++ Works/Python/Python-day-02.py
@@ -18,6 +18,42 @@ def somemethod(*ededler):
 somemethod(2,4,5)
 
 ededler=[1,2,3,4,5]
+
 def somemethod():
     print(len(ededler))
 somemethod()
+
+def func():
+    for i in range(20):
+        print(i)
+func()
+
+a=5
+print(type(a))
+
+ededler=[1,2,3,'salam',True]
+for element in ededler:
+    if type(element)==int:
+        print(element ,' ededdir')
+    else:
+        print(element, 'eded deyil')
+
+ededler=[1,2,3,'salam',True]
+eded=[]
+string=[]
+Bool=[]
+
+for element in ededler:
+    if type(element)==int:
+        eded.append(element)
+
+for element in ededler:
+    if type(element)==str:
+        string.append(element)
+    
+for element in ededler:
+    if type(element)==bool:
+        Bool.append(element)
+print(eded)
+print(string)
+print(Bool)

49cf103e2f407b66fefc5871e1ac9d3c83a1379e
omarseyyad <seyyad_omerov@mail.ru>
2021-12-06 17:12:06 +0400

    HTML,CSS,JS texnologiyalarindan istifade etdim

---
+++ FinalTasks/Task02/main.js
@@ -0,0 +1,8 @@
+function imgslider(anything){
+document.querySelector('.pepsim').src = anything;
+}
+
+function changeBgcolor(color){
+const bgsite = document.querySelector('.bgsite');
+bgsite.style.background = color;
+}  
--- Works/Python/Python-day-09.py
+++ Works/Python/Python-day-09.py
@@ -1,5 +1,3 @@
-
-# istifadecinin qeydiyyatdan kecmesi ucun lazim olan kodlar da burda olacaq
 from data import User,users
 
 def registerUser():
@@ -7,7 +5,5 @@ def registerUser():
     soyad=input('Soyadiniz :')
     username=input('Istifadeci adiniz :')
     password=input('Sifreniz :')
-    # terminaldan daxil edilen melumatlar esasinda yeni user obyekti yarat
     user=User(ad,soyad,username,password)
-    # yaradilan yeni useri users listine elave et
     users.append(user)
